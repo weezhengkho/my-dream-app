0e4b174dc187a9805083b3dfbb50ce5276fb0c12
weezheng <khoweezheng@gmail.com>
2017-05-24 22:55:44 +0800

    first commit
    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
